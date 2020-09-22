b94927023ab40695452faf69d3733ee370cbf7cc
danieltrut <Avdayg7410>
2020-09-22 11:46:43 +0300

    Ylesanne 1.5

---
+++ 1/Ylesanne 5.py
@@ -0,0 +1,8 @@
+#kasutaja sisend
+aine_punktid = int(input("Sisestage aine punktide arv: "))
+nadalate_arv = int(input("Sisestage nadalate arv: "))
+#arvutused
+summa = (aine_punktid * nadalate_arv) / 10
+
+#valjastus
+print(round(summa))

f75eb2e557063f8a20e9feb6e04c0c4ca0f66323
danieltrut <Avdayg7410>
2020-09-22 11:00:37 +0300

    Ylesanne 1.3

--- 1/Ylesanne 3.py
+++ 1/Ylesanne 3.py
@@ -2,8 +2,12 @@ alus = input("Sisestage astme alus: ")
 astendaja = input("Sisestage astendaja: ")
 print(type(alus))
 print(type(astendaja))
-#teisendame tuupi
 
-alus_int = int(alus)
-print(type(alus_int))
-print(type(astendaja))
+
+#arvutused
+
+tulemus = alus ** astendaja
+
+
+#valjastatus
+print(tulemus)

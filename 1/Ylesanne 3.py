7175656a822504603a1bd14c5382ea88a478634a
danieltrut <Avdayg7410>
2020-09-22 10:52:14 +0300

    Koostatudylesanne 1.2 lahendus

---
+++ 1/Ylesanne 3.py
@@ -0,0 +1,8 @@
+alus = input("Sisestage astme alus: ")
+astendaja = input("Sisestage astendaja: ")
+print(type(alus))
+print(type(astendaja))
+#teisendame tuupi
+alus_int
+print(type(alus))
+print(type(astendaja))

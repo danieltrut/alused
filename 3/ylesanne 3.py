b4184131c284cf7335db5e69e30ffc229f1fa42c
danieltrut <Avdayg7410>
2020-10-01 11:47:13 +0300

    Ylesanne 3.3

---
+++ 3/ylesanne 3.py
@@ -0,0 +1,16 @@
+# juhuslikke taisarvude genereerimis
+                from random import randit
+#kasutaja sisend
+soovitud = int(input("Sisestage  täringute arvu: "))
+
+#viskame taringut miikaua, kui juhuslikult tulnud arv on sama mis on soovitud
+# selleks paneme korrad lugema
+while(kord <= soovitud_kord):
+    # teeme vise
+    taring = randint(1, 6)
+    # kontrol kui palju tuli
+    print(taring)
+    # kontrol kui palju tuli
+    kord += 1
+
+

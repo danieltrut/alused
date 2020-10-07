0afea587af0b2c0fff517346701a64f97d5f101f
danieltrut <Avdayg7410>
2020-10-07 12:05:59 +0300

    Ylesanne 2.1

---
+++ 2/ylesanne 2.1.py
@@ -0,0 +1,10 @@
+#kasutaja sisend
+sisestatud_temperatuur = int(input("Sisesta ohu temperatuur: "))
+
+#arvestused
+if sisestatud_temperatuur > 4:
+    print("Ei ole jäätumise ohtu")
+else:
+    print("On jäätumise oht")
+
+#valjastus

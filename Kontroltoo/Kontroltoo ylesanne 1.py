6f076747f76b2789e3f3dcf3920a9476dca43306
danieltrut <Avdayg7410>
2020-10-28 16:36:32 +0200

    kontroltoo

---
+++ Kontroltoo/Kontroltoo ylesanne 1.py
@@ -0,0 +1,16 @@
+#kasutaja sisend
+koht = input("kus toimub finaalturniiride toimumiskoht: ")
+turniiri_kogus = input("kui palju turniiri toimus: ")
+
+turniiridel_osalenud_naiskondade_arvud = [12, 12, 16, 16, 16, 16, 24]
+
+if (turniiridel_osalenud_naiskondade_arvud <= 15):
+        tugiisikute_arv == 10
+elif (turniiridel_osalenud_naiskondade_arvud > 15):
+        tugiisikute_arv == 8
+
+
+inimeste_arv = naiskondade_arv * (22 + tugiisikute_arv)
+
+
+
